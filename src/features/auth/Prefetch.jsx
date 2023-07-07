5c91b4998797f44c682c279013d22d985570bd26
Jerry Ren <jerryren.5279@gmail.com>
2023-07-07 19:48:40 -0400

    added jobs subscription to Prefetch

--- src/features/auth/Prefetch.jsx
+++ src/features/auth/Prefetch.jsx
@@ -3,17 +3,20 @@ import { store } from "../../app/store";
 import { usersApiSlice } from "../users/usersApiSlice";
 import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
+import { jobsApiSlice } from "../jobs/jobsApiSlice";
 
 // Wrap Prefetch component around the routes that you want the state to be prefetched for.
 const Prefetch = () => {
 	useEffect(() => {
 		console.log("subscribing");
 		// create a manual subscription to users slice using .initiate(). The subscription will remain active indefinitely (until we leave the protected pages).
-		const subscription = store.dispatch(usersApiSlice.endpoints.getUsers.initiate());
+		const users = store.dispatch(usersApiSlice.endpoints.getUsers.initiate());
+		const jobs = store.dispatch(jobsApiSlice.endpoints.getJobs.initiate());
 
 		return () => {
 			console.log("unsubscribing");
-			subscription.unsubscribe();
+			users.unsubscribe();
+			jobs.unsubscribe();
 		};
 	}, []);
 
