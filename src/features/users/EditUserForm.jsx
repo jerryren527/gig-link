c1612df33b7d3e33887b0c06144fb533eff65cbc
Jerry Ren <jerryren.5279@gmail.com>
2023-07-08 00:32:26 -0400

    minor syntax change in EditUserForm

--- src/features/users/EditUserForm.jsx
+++ src/features/users/EditUserForm.jsx
@@ -78,35 +78,35 @@ const EditUserForm = ({ user }) => {
         <div className='edit-user-form__input'>
           <label htmlFor='username-input'>Username: </label>
           <input id="username-input" value={username} type='text' onChange={(e) => setUsername(e.target.value)} />
+        </div>
 
-          <div className='edit-user-form__input'>
-            <label htmlFor='password-input'>Password: </label>
-            <input id="password-input" value={password} type='password' onChange={(e) => setPassword(e.target.value)} />
-          </div>
+        <div className='edit-user-form__input'>
+          <label htmlFor='password-input'>Password: </label>
+          <input id="password-input" value={password} type='password' onChange={(e) => setPassword(e.target.value)} />
+        </div>
 
-          <div className='edit-user-form__input'>
-            <label htmlFor='firstName-input'>First Name: </label>
-            <input id="firstName-input" value={firstName} type='text' onChange={(e) => setFirstName(e.target.value)} />
-          </div>
+        <div className='edit-user-form__input'>
+          <label htmlFor='firstName-input'>First Name: </label>
+          <input id="firstName-input" value={firstName} type='text' onChange={(e) => setFirstName(e.target.value)} />
+        </div>
 
-          <div className='edit-user-form__input'>
-            <label htmlFor='lastName-input'>Last Name: </label>
-            <input id="lastName-input" value={lastName} type='text' onChange={(e) => setLastName(e.target.value)} />
-          </div>
+        <div className='edit-user-form__input'>
+          <label htmlFor='lastName-input'>Last Name: </label>
+          <input id="lastName-input" value={lastName} type='text' onChange={(e) => setLastName(e.target.value)} />
+        </div>
 
-          <div className='edit-user-form__input'>
-            <label htmlFor='role-input'>Role: </label>
-            <select id="role-input" value={role} onChange={(e) => setRole(e.target.value)}>
-              <option value="">-- Select --</option>
-              <option value="Client">Client</option>
-              <option value="Freelancer">Freelancer</option>
-              <option value="Admin">Admin</option>
-            </select>
-          </div>
+        <div className='edit-user-form__input'>
+          <label htmlFor='role-input'>Role: </label>
+          <select id="role-input" value={role} onChange={(e) => setRole(e.target.value)}>
+            <option value="">-- Select --</option>
+            <option value="Client">Client</option>
+            <option value="Freelancer">Freelancer</option>
+            <option value="Admin">Admin</option>
+          </select>
+        </div>
 
-          <div className='btn'>
-            <button type='submit'>Save</button>
-          </div>
+        <div className='btn'>
+          <button type='submit'>Save</button>
         </div>
 
       </form>
